ba05b1eb7d0246e8ecabfa6fc0696c57f4048ef8
Rene Stein <rene@renestein.net>
2018-12-12 12:34:48 +0100

    Added hint

--- ConfigureAwaitEnforcer/ConfigureAwaitEnforcer/ConfigureAwaitEnforcerAnalyzer.cs
+++ ConfigureAwaitEnforcer/ConfigureAwaitEnforcer/ConfigureAwaitEnforcerAnalyzer.cs
@@ -1,8 +1,7 @@
 using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
-using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -17,35 +16,51 @@ namespace ConfigureAwaitEnforcer
 
     // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
     // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
-    private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
-    private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
-    private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
-    private const string Category = "Naming";
+    private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle),
+      Resources.ResourceManager, typeof(Resources));
 
-    private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+    private static readonly LocalizableString MessageFormat =
+      new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager,
+        typeof(Resources));
 
-    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+    private static readonly LocalizableString Description =
+      new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager,
+        typeof(Resources));
+
+    private const string Category = "ConfigureAwait";
+
+    private static readonly DiagnosticDescriptor RULE = new DiagnosticDescriptor(DiagnosticId,
+      Title,
+      MessageFormat,
+      Category,
+      DiagnosticSeverity.Warning,
+      isEnabledByDefault: true,
+      description: Description);
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(RULE);
 
     public override void Initialize(AnalysisContext context)
     {
-      // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
-      // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
-      context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+      context.RegisterSyntaxNodeAction(AnalyzeSymbol,
+        SyntaxKind.AwaitExpression);
     }
 
-    private static void AnalyzeSymbol(SymbolAnalysisContext context)
+    private static void AnalyzeSymbol(SyntaxNodeAnalysisContext context)
     {
-      // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
-      var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
+      const string CONFIGURE_AWAIT_METHOD_NAME = nameof(Task.ConfigureAwait);
+      var currentAwait = (AwaitExpressionSyntax)context.Node;
 
-      // Find just those named type symbols with names containing lowercase letters.
-      if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
+      var hasConfigureAwait = currentAwait
+        .DescendantTokens()
+        .Any(token => token.Value.ToString().Equals(CONFIGURE_AWAIT_METHOD_NAME,
+          StringComparison.Ordinal));
+
+      if (!hasConfigureAwait)
       {
-        // For all such symbols, produce a diagnostic.
-        var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
-
+        var diagnostic = Diagnostic.Create(RULE, currentAwait.GetLocation());
         context.ReportDiagnostic(diagnostic);
       }
+
     }
   }
 }
